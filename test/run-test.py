560b978fed4f82e2b76695ab2571c8397870efea
zk33 <ymzktkak@gmail.com>
2013-03-26 21:29:43 +0900

    update tests files

---
+++ test/run-test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys,os
+sys.path.insert(0, os.path.abspath(os.path.split(os.path.dirname(__file__))[0]))
+
+import unittest
+import negi
+
+class NegiTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    #write test soon...^^;
+
+
+
+
+
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
