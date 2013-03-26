ab3c45589ccd593f7f8eb35d44c7aa2fe5f27638
zk33 <ymzktkak@gmail.com>
2013-03-26 21:30:06 +0900

    fix command line entry point

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
     long_description=read('README.rst'),
     install_requires=["aaargh","jinja2"],
-    entry_points={'console_scripts': ["negi = negi:main"]},
+    entry_points={'console_scripts': ["negi = negi.main:main"]},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
