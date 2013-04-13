41514380366c230da87a166993588b70321a6a9c
zk33 <ymzktkak@gmail.com>
2013-04-13 19:28:20 +0900

    fix command line help message

--- negi/main.py
+++ negi/main.py
@@ -7,8 +7,8 @@ app = aaargh.App(description="Jinja2+JSON powered static HTML build tool")
 
 @app.cmd(help='Parse JSON and build HTML')
 @app.cmd_arg('-d','--data_dir',default='./data',help='JSON data dirctory(default:./data')
-@app.cmd_arg('-t','--tmpl_dir',default='./templates',help='JSON data dirctory(default:./templates')
-@app.cmd_arg('-o','--out_dir',default='./dist',help='out dirctory(default:./dist')
+@app.cmd_arg('-t','--tmpl_dir',default='./templates',help='Jinja2 template dirctory(default:./templates')
+@app.cmd_arg('-o','--out_dir',default='./dist',help='Output dirctory(default:./dist')
 @app.cmd_arg('-v','--verbose',nargs='?',const=True,default=False)
 def build(data_dir,tmpl_dir,out_dir,verbose):
 
